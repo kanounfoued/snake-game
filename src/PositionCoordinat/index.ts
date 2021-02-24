2b3571f7052d79ffa71fc9776509090cd48ddfa7
kanounfoued <fouad.kanoun@legal-doctrine.com>
2021-02-24 14:18:50 +0100

    :hammer: update the name of type position

---
+++ src/PositionCoordinat/index.ts
@@ -0,0 +1,27 @@
+module.exports = class PositionCoordinat {
+  private x: number;
+  private y: number;
+
+  constructor(x: number = 0, y: number = 0) {
+    this.x = x;
+    this.y = y;
+  }
+
+  getX(): number {
+    return this.x;
+  }
+
+  getY(): number {
+    return this.y;
+  }
+
+  setX(x: number): void {
+    this.x = x;
+  }
+
+  setY(y: number): void {
+    this.y = y;
+  }
+};
+
+export {};
