1f619b4f819039ff495cbbe9d31c74dc2cba714a
kanounfoued <fouad.kanoun@legal-doctrine.com>
2021-02-27 00:43:56 +0100

    :lipstick: add button to control the snack

--- src/index.ts
+++ src/index.ts
@@ -6,14 +6,29 @@ const game = new Game();
 rootDiv.append(game.render());
 
 // setInterval(() => {
-//   game.snack.moveTo(1);
+//   game.snack.moveTo(2);
 //   game.render();
 // }, 50);
 
-const button = document.getElementById("right");
+const buttonRight = document.getElementById("right");
+const buttonLeft = document.getElementById("left");
+const buttonBottom = document.getElementById("bottom");
+const buttonTop = document.getElementById("top");
 
-button.onclick = () => {
+buttonRight.onclick = () => {
   game.snack.moveTo(1);
+  game.render();
+};
+buttonLeft.onclick = () => {
+  game.snack.moveTo(0);
+  game.render();
+};
+buttonBottom.onclick = () => {
+  game.snack.moveTo(3);
+  game.render();
+};
+buttonTop.onclick = () => {
+  game.snack.moveTo(2);
   game.render();
 };
 
